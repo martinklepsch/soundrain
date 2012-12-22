9022063e99f3c2bb9e25b579eb126653e9d222ce
Tom Nick <Tom.w.a.nick@gmail.com>
2012-12-22 17:07:00 +0100

    reading mp3's works, code is from: https://gist.github.com/1968570

--- src/scripper/core.clj
+++ src/scripper/core.clj
@@ -1,8 +1,26 @@
 (ns scripper.core
-
-(:import [org.jaudiotagger.audio AudioFileIO]))
-
+  (:import [org.jaudiotagger.audio AudioFileIO]
+           [org.jaudiotagger.tag FieldKey]))
+           
+(defn tags [file]
+  (let [fields (apply conj {} (map (fn [n] [(keyword (. (. n toString) toLowerCase)) n]) (. FieldKey values)))
+	tag (. file (getTag))]
+    (apply conj {}
+	    (filter (fn [[name val]] (and val (not (empty? val))))
+		    (map (fn [[name val]]
+			  [name (seq (map #(. % getContent) (. tag (getFields val))))])
+		      fields)))))
+ 
+(defn audioheader [file]
+  (bean (. file (getAudioHeader))))
+ 
+(defn metadata [filename]
+  (let [file (AudioFileIO/read (new java.io.File filename))]
+    {:tags (tags file)
+     :audioheader (audioheader file)}))
+                  
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println "Hello, World!"))
+  (print (:artist (:tags (metadata "./ressources/test_01_What_i_hope.mp3")))))
+
