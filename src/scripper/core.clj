9b08ffe173a8ab36307400f7b3f50e235df78364
Tom Nick <Tom.w.a.nick@gmail.com>
2012-12-23 15:10:22 +0100

    Tagging of a mp3 works, picture is missing

--- src/scripper/core.clj
+++ src/scripper/core.clj
@@ -18,9 +18,31 @@
   (let [file (AudioFileIO/read (new java.io.File filename))]
     {:tags (tags file)
      :audioheader (audioheader file)}))
-                  
+     
+(defn tagmp3 [tags,filename]
+  ;; tags the file with the name "filename" with tags
+  (let [
+  file (AudioFileIO/read (new java.io.File filename))
+  tag (. file getTag)]
+    (. tag setField FieldKey/ARTIST (str(:artist tags)))
+    (. tag setField FieldKey/TITLE  (str(:title tags)))
+    (. tag setField FieldKey/YEAR   (str(:year tags)))
+    (. tag setField FieldKey/ALBUM  (str(:album tags)))
+    (. file commit)))
+
+(defn createmp3 [mp3,filename]
+  ;; don't know if there is a better way than writing the mp3 to disk and tag it.
+  ;; mp3 should be a byte-array, maybe function can be rewritten with clojure IO
+  (let [
+    file (java.nio.channels.FileChannel/open (java.nio.file.Paths/get filename) (into-array '(java.nio.file.StandardOpenOption/CREATE_NEW java.nio.file.StandardOpenOption/WRITE)))]
+    (. file write mp3)
+    (. file close)))
+
+(defn test-write []
+  (tagmp3 {:artist "TestArtist", :title "TestTrack", :album "TestAlbum", :year "1000"} "./ressources/test_01_What_i_hope.mp3"))
+  
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (print (:artist (:tags (metadata "./ressources/test_01_What_i_hope.mp3")))))
+  (test-write))
 
