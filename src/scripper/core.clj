d79a0f12d02ceb5345cddf861c856ef8c8698111
Tom Nick <Tom.w.a.nick@gmail.com>
2012-12-23 19:12:01 +0100

    Will now fetch the mp3 of a given url and tag it with the given parameters :),  only refactoring has to be done

--- src/scripper/core.clj
+++ src/scripper/core.clj
@@ -6,7 +6,8 @@
            [org.jaudiotagger.tag.id3 	ID3v23Frame]
            [org.jaudiotagger.tag.datatype DataTypes]
            [org.jaudiotagger.tag.reference PictureTypes]
-           [org.jaudiotagger.tag TagField])
+           [org.jaudiotagger.tag TagField]
+           [org.jaudiotagger.audio.mp3 MP3File])
   (:require[clojure.java.io :as io]))
            
 (defn tags [file]
@@ -33,19 +34,16 @@
   frame (new ID3v23Frame "APIC")]
     (. body setObjectValue DataTypes/OBJ_PICTURE_DATA image)
     (. body setObjectValue DataTypes/OBJ_PICTURE_TYPE PictureTypes/DEFAULT_ID)
-    (. body setObjectValue DataTypes/OBJ_MIME_TYPE (str "image/jpg"))
-    (. body setObjectValue DataTypes/OBJ_DESCRIPTION (str ""))
+    (. body setObjectValue DataTypes/OBJ_MIME_TYPE "image/jpg")
+    (. body setObjectValue DataTypes/OBJ_DESCRIPTION "")
     (. frame setBody body)
     frame))
 
 (defn createmp3 [mp3,filename]
   ;; don't know if there is a better way than writing the mp3 to disk and tag it.
   ;; mp3 should be a byte-array, maybe function can be rewritten with clojure IO
-  (let [file (java.nio.channels.FileChannel/open 
-	  (java.nio.file.Paths/get filename) 
-	  (into-array '(java.nio.file.StandardOpenOption/CREATE_NEW java.nio.file.StandardOpenOption/WRITE)))]
-    (. file write mp3)
-    (. file close)))
+  (with-open [output (new java.io.FileOutputStream filename)]
+    (.write output mp3)))
     
 (defn test-image-array []
   (let [
@@ -56,35 +54,36 @@
     (. filestream close)
     data))
     
-(defn download-image [url]
+(defn download-binary [url]
   (let [
     u (new java.net.URL url)
     filestream (. u openStream)]
     (. org.apache.commons.io.IOUtils toByteArray filestream)))
     
-(defn tagmp3 [tags,filename]
+(defn tagmp3 [tags]
   ;; tags the file with the name "filename" with tags
   (let [
+  filename (str "./" (:artist tags) " - " (:title tags) ".mp3")
+  jfile (createmp3 (download-binary (:mp3 tags)) filename)
   file (AudioFileIO/read (new java.io.File filename))
-  tag (. file getTag)]
+  tag (. file getTagOrCreateAndSetDefault)]
     (. tag setField FieldKey/ARTIST (:artist tags))
     (. tag setField FieldKey/TITLE  (:title tags))
     (. tag setField FieldKey/YEAR   (:year tags))
     (. tag setField FieldKey/ALBUM  (:album tags))
-    (. tag deleteArtworkField)
-    (. tag setField (set-image (download-image (:image tags))))
+    (. tag setField (set-image (download-binary (:image tags))))
     (. file commit)))
     
     
 (defn test-write []
   (tagmp3 {
-    :artist "TestArtist", 
-    :title "TestTrack", 
-    :album "TestAlbum", 
-    :year "1000" 
-    :image "https://dl.dropbox.com/u/1994140/P8270580n.jpg"
-    } 
-    "./ressources/test_01_What_i_hope.mp3"))
+    :mp3 	"https://dl.dropbox.com/u/1994140/testmp3.mp3"
+    :artist 	"TestArtist", 
+    :title 	"TestTrack", 
+    :album 	"TestAlbum", 
+    :year 	"1000" 
+    :image 	"https://dl.dropbox.com/u/1994140/P8270580n.jpg"
+    } ))
 
       
 (defn -main
