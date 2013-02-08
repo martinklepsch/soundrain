704818d1ee53e1f64bef3de3eaa519ab829fe484
Tom Nick <Tom.W.A.Nick@gmail.com>
2013-02-08 15:46:16 +0100

    Added trailing-slash-handler

--- src/scripper/util.clj
+++ src/scripper/util.clj
@@ -1,8 +1,33 @@
- (ns scripper.util
-   (:import [org.apache.commons.io IOUtils]))
+(ns scripper.util
+  (:import [org.apache.commons.io IOUtils])
+  (:require [compojure.handler :as handler]))
+  
+  (defn download-binary [url]
+    "downloads a file and returns it as a bytearray"
+    (let [stream     (new java.net.URL url)
+          filestream (.openStream stream)]
+      (IOUtils/toByteArray filestream)))
 
- (defn download-binary [url]
-   "downloads a file and returns it as a bytearray"
-   (let [stream     (new java.net.URL url)
-         filestream (.openStream stream)]
-     (IOUtils/toByteArray filestream)))
+  (defn with-uri-rewrite
+  "Rewrites a request uri with the result of calling f with the
+   request's original uri.  If f returns nil the handler is not called."
+  [handler f]
+  (fn [request]
+    (let [uri (:uri request)
+          rewrite (f uri)]
+      (if rewrite
+        (handler (assoc request :uri rewrite))
+        nil))))
+  
+  (defn- uri-snip-slash
+    "Removes a trailing slash from all uris except \"/\"."
+    [uri]
+    (if (and (not (= "/" uri))
+             (.endsWith uri "/"))
+      (subs uri 0 (dec (count uri)))
+      uri))
+  
+  (defn ignore-trailing-slash
+    "Makes routes match regardless of whether or not a uri ends in a slash."
+    [handler]
+    (with-uri-rewrite handler uri-snip-slash))
