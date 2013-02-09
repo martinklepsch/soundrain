e7bd9318ceec88af9847f35d35b610dcf5dd22c9
Martin Klepsch <martinklepsch@gmail.com>
2013-02-09 21:27:24 +0100

    adds ganalytics tracking

--- src/scripper/view.clj
+++ src/scripper/view.clj
@@ -14,7 +14,17 @@
         (include-css "/css/style.css")
         (include-css "http://fonts.googleapis.com/css?family=Open+Sans")
         (include-js "/script/base64-binary.js")
-      	(include-js "/script/scripper.js")]
+      	(include-js "/script/scripper.js")
+        [:script {:type "text/javascript"}
+          "var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-3138561-9']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();"]
       [:body
         [:div.container-fluid
             [:div.row body]
