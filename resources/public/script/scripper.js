8a7df49af3bfc8fa561ca50d555b29a267594c10
Martin Klepsch <martinklepsch@gmail.com>
2013-02-22 21:22:33 +0100

    use underscores in filenames and wording

--- resources/public/script/scripper.js
+++ resources/public/script/scripper.js
@@ -182,7 +182,7 @@ function handle_binary_data(evt, current_index) {
   // Copied code
   var binary_tag = Base64Binary.decodeArrayBuffer(currently_processed_data[right_index].tag);
 
-  var filename = currently_processed_data[right_index].artist + " - " + currently_processed_data[right_index].title + ".mp3".replace(/\s/g, "_");;
+  var filename = (currently_processed_data[right_index].artist + " - " + currently_processed_data[right_index].title + ".mp3").replace(/\s/g, "_");;
 
   // Actually write to the filesystem
   filesystem.root.getFile(filename, {create: true}, function(file_entry) {
