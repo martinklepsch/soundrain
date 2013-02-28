5f4083079e67ecf91ba1fc65264e9252737ee580
conchulio <waltersheridan@gmail.com>
2013-02-28 20:28:30 +0100

    Added favicon

--- src/scripper/view.clj
+++ src/scripper/view.clj
@@ -24,7 +24,8 @@
             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();"]]
+          })();"]
+        [:link {:rel "shortcut icon" :href "/favicon.ico"}]]
       [:body
         [:div.container-fluid
             [:div.row body]
