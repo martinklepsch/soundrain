42f76165dadd058d80e2089590e869c48ed6d386
conchulio <waltersheridan@gmail.com>
2013-03-12 19:04:21 +0100

    Changed logotext font to Ubuntu like the rest of the page

--- src/soundrain/view.clj
+++ src/soundrain/view.clj
@@ -15,7 +15,7 @@
       js = d.createElement(s); js.id = id;
       js.src = '//connect.facebook.net/en_US/all.js#xfbml=1';
       fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));"]))
+    } (document, 'script', 'facebook-jssdk'));"]))
 
 (defn fb-like [] 
   (html [:div.fb-like 
