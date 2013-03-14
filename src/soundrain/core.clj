61e6d63c507065623f6a4fb8bc51cbb0754ae216
Tom Nick <Tom.w.a.nick@gmail.com>
2013-03-14 21:17:44 +0100

    Image are now sent as b64 encoded bytearrays, I'll add high-res pictures later

--- src/soundrain/core.clj
+++ src/soundrain/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.io :as io]
             [soundrain.metadata :as metadata]
             [soundrain.parse :as parse]
+            [soundrain.util :as util]
             [ring.util.codec :as codec]
             [soundrain.view :as view])
   (:use compojure.core [hiccup core page form util]))
@@ -16,9 +17,10 @@
           ;            (comp (partial hash-map :tag) codec/base64-encode metadata/create-ID3v23-tag) 
           ;            mp3-metadata)
           html-site (map (comp (partial hash-map :html) view/song-form) 
-                          mp3-metadata (range))]
+                          mp3-metadata (range))
+          images (map (comp (partial hash-map :image-data) codec/base64-encode util/download-binary :image) mp3-metadata)]
       ; (map merge mp3-metadata mp3-tags html-site))
-      (map merge mp3-metadata html-site))
+      (map merge mp3-metadata html-site images))
   ; Return empty list when an error occurs
   ; For example when a user enters a url 
   ; but the corresponding page on soundcloud
