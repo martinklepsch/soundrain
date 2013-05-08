3f09a6b75aeb45835268e6a518ce45d16d1f46de
conchulio <waltersheridan@gmail.com>
2013-05-08 15:43:38 +0200

    Fixed a bug, when there was no image to be found, set our logo as default picture

--- src/soundrain/util.clj
+++ src/soundrain/util.clj
@@ -2,12 +2,13 @@
   (:import [org.apache.commons.io IOUtils])
   (:require [compojure.handler :as handler])
   (:use [clojure.string :only [replace]]))
-  
+
   (defn download-binary [url]
     "downloads a file and returns it as a bytearray"
-    (let [stream     (new java.net.URL url)
+    (try (let [stream     (new java.net.URL url)
           filestream (.openStream stream)]
-      (IOUtils/toByteArray filestream)))
+      (IOUtils/toByteArray filestream))
+    (catch Exception e (download-binary "http://soundrain.org/images/logotext.png"))))
 
   (defn with-uri-rewrite
   "Rewrites a request uri with the result of calling f with the
@@ -19,7 +20,7 @@
       (if rewrite
         (handler (assoc request :uri rewrite))
         nil))))
-  
+
   (defn- uri-snip-slash
     "Removes a trailing slash from all uris except \"/\"."
     [uri]
@@ -27,15 +28,15 @@
              (.endsWith uri "/"))
       (subs uri 0 (dec (count uri)))
       uri))
-  
+
   (defn ignore-trailing-slash
     "Makes routes match regardless of whether or not a uri ends in a slash."
     [handler]
     (with-uri-rewrite handler uri-snip-slash))
-  
+
   (defn unescape-string
     "Takes a string and unescapes it, also replaces / with |"
   	[s]
-  	(replace s 
-             #"&quot;|&gt;|&lt;|\/" 
+  	(replace s
+             #"&quot;|&gt;|&lt;|\/"
              {"&quot;" "&", "&gt;" ">", "&lt;" "<", "/" "|"}))
